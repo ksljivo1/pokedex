8b12431c9e748eb4d2c19158ce937fb915d8e38c
ksljivo1 <ksljivo1@etf.unsa.ba>
2024-10-18 20:33:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Header} from "./components/Header.jsx";
+import {SideNav} from "./components/SideNav.jsx";
+import {PokeCard} from "./components/PokeCard.jsx";
+
+function App() {
+
+  return (
+      <>
+          <Header />
+          <SideNav />
+          <PokeCard />
+      </>
+  )
+}
+
+export default App
